ecd49e080832c8f0e6ad0973fc33e9228b6f3e4c
tyarchy <ty.d.gregory@gmail.com>
2022-02-28 18:12:53 -0700

    query the database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,10 +8,41 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+  // Connect to database
+  const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '^n5MRAJEteqG',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+  // Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+if (err) {
+console.log(err);
+}
+console.log(result);
+});
+
+  
   // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
   });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
